c433e3ae944b7dbf6cdeb5e8c54252ecfe3b86c9
Omar Cossio <omar119mce@gmail.com>
2021-01-14 22:02:12 -0500

    added to about page and navigation

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
 
     return (
-        <div className="container ml-5">
+        <div className="container ml-5 mt-5">
             <div className="row">
                 <div className="col-9">
                     <div className="bg-secondary text-white">
@@ -21,7 +21,26 @@ function About() {
 
                                             <p> My name is Omar Cossio. I am originally from Mexico City, Mexico,
                                             but grew up in Asheboro, NC. I moved to Charlotte, NC in 2011, and love it.
-                </p>
+                                            </p>
+                                            <div>
+                                                <p>
+                                                    I graduated from Johnson C. Smith University in 2016 and currently I am studying at The Coding Bootcamp at UNC Charlotte, where I'm working to become a full-stack developer. 
+                                                </p>
+                                                <p>
+                                                    Click on the "Portfolio" tab to take a look at some of my projects and join me as I continue to add to my portfolio. 
+                                                </p>
+
+                                                <h5>A Few Reasons I've Enjoyed Coding</h5>
+                                        <p>
+                                            <ul>
+                                                <li>I enjoy creating</li>
+                                                <li>It's ever changing</li>
+                                                <li>I'm a life-long learner</li>
+                                                <li>It can be like solving a puzzle</li>   
+                                            </ul>
+                                        </p>
+
+                                            </div>
 
 
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,6 +8,8 @@ function Navigation() {
 
     return (
         <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <span className="nav-brand text-white">Omar Cossio</span>
             <ul className="nav nav-tabs">
                 <li className="nav-item">
                     <Link
@@ -34,7 +36,42 @@ function Navigation() {
         </Link>
                 </li>
             </ul>
+
+            </nav>
         </div>
+
+
+
+
+
+        // <div>
+        //     <ul className="nav nav-tabs">
+        //         <li className="nav-item">
+        //             <Link
+        //                 to="/"
+        //                 className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
+        //             >
+        //                 Home
+        // </Link>
+        //         </li>
+        //         <li className="nav-item">
+        //             <Link
+        //                 to="/portfolio"
+        //                 className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+        //             >
+        //                 Portfolio
+        // </Link>
+        //         </li>
+        //         <li className="nav-item">
+        //             <Link
+        //                 to="/contact"
+        //                 className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+        //             >
+        //                 Contact
+        // </Link>
+        //         </li>
+        //     </ul>
+        // </div>
     )
 }
 
