352d82bca3e2b34682b1264dac7738679f60f32b
Omar Cossio <omar119mce@gmail.com>
2021-01-14 23:43:46 -0500

    fixing bugs

--- src/components/About.js
+++ src/components/About.js
@@ -9,47 +9,41 @@ function About() {
                     <div className="bg-secondary text-white">
                         <h2>About Omar</h2>
                         <hr></hr>
-
-                        <section className="main">
+                        <div className="main">
                             <div className="container">
                                 <div className="row">
                                     <div className="col-12 pb-3">
                                         <div className="image-wrapper float-left pr-3">
-                                            <img scr="myPhoto.jpeg" alt="Omar's Profile Photo"></img>
+                                            <img src="myPhoto.png" alt="Omar's Profile Photo" width="30%"></img>
                                         </div>
-                                        <div className="content-wrapper">
-
+                                        <div className="content-wrapper float-end">
                                             <p> My name is Omar Cossio. I am originally from Mexico City, Mexico,
                                             but grew up in Asheboro, NC. I moved to Charlotte, NC in 2011, and love it.
                                             </p>
                                             <div>
                                                 <p>
-                                                    I graduated from Johnson C. Smith University in 2016 and currently I am studying at The Coding Bootcamp at UNC Charlotte, where I'm working to become a full-stack developer. 
+                                                    I graduated from Johnson C. Smith University in 2016 and currently I am studying at The Coding Bootcamp at UNC Charlotte, where I'm working to become a full-stack developer.
                                                 </p>
                                                 <p>
-                                                    Click on the "Portfolio" tab to take a look at some of my projects and join me as I continue to add to my portfolio. 
+                                                    Click on the "Portfolio" tab to take a look at some of my projects and join me as I continue to add to my portfolio.
                                                 </p>
 
                                                 <h5>A Few Reasons I've Enjoyed Coding</h5>
-                                        <p>
-                                            <ul>
-                                                <li>I enjoy creating</li>
-                                                <li>It's ever changing</li>
-                                                <li>I'm a life-long learner</li>
-                                                <li>It can be like solving a puzzle</li>   
-                                            </ul>
-                                        </p>
+                                                <p>
+                                                    <ul>
+                                                        <li>I enjoy creating</li>
+                                                        <li>It's ever changing</li>
+                                                        <li>I'm a life-long learner</li>
+                                                        <li>It can be like solving a puzzle</li>
+                                                    </ul>
+                                                </p>
 
                                             </div>
-
-
-
-
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </section>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,38 +8,38 @@ function Navigation() {
 
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">Omar Cossio</a>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
+                <a className="navbar-brand">Omar Cossio</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
                             <Link
                                 to="/"
                                 className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>
                                 Home
                         </Link>
                         </li>
-                        <li class="nav-item">
-                        <Link
-                                    to="/portfolio"
-                                    className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
-                                    Portfolio
+                        <li className="nav-item">
+                            <Link
+                                to="/portfolio"
+                                className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+                                Portfolio
                         </Link>
                         </li>
-                        <li class="nav-item dropdown">
-                        <Link
-                                    to="/contact"
-                                    className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
-                                    Contact
+                        <li className="nav-item dropdown">
+                            <Link
+                                to="/contact"
+                                className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+                                Contact
                         </Link>
                         </li>
                     </ul>
                 </div>
             </nav>
+
         </div>
 
     )
