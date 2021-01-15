1e4fb79418c35dd12145d2232710ecf807d22b93
Omar Cossio <omar119mce@gmail.com>
2021-01-14 22:57:29 -0500

    updated navbar styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,70 +8,40 @@ function Navigation() {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <span className="nav-brand text-white">Omar Cossio</span>
-            <ul className="nav nav-tabs">
-                <li className="nav-item">
-                    <Link
-                        to="/"
-                        className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
-                    >
-                        Home
-        </Link>
-                </li>
-                <li className="nav-item">
-                    <Link
-                        to="/portfolio"
-                        className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
-                    >
-                        Portfolio
-        </Link>
-                </li>
-                <li className="nav-item">
-                    <Link
-                        to="/contact"
-                        className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-                    >
-                        Contact
-        </Link>
-                </li>
-            </ul>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">Omar Cossio</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <Link
+                                to="/"
+                                className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>
+                                Home
+                        </Link>
+                        </li>
+                        <li class="nav-item">
+                        <Link
+                                    to="/portfolio"
+                                    className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+                                    Portfolio
+                        </Link>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <Link
+                                    to="/contact"
+                                    className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+                                    Contact
+                        </Link>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>
 
-
-
-
-
-        // <div>
-        //     <ul className="nav nav-tabs">
-        //         <li className="nav-item">
-        //             <Link
-        //                 to="/"
-        //                 className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
-        //             >
-        //                 Home
-        // </Link>
-        //         </li>
-        //         <li className="nav-item">
-        //             <Link
-        //                 to="/portfolio"
-        //                 className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
-        //             >
-        //                 Portfolio
-        // </Link>
-        //         </li>
-        //         <li className="nav-item">
-        //             <Link
-        //                 to="/contact"
-        //                 className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-        //             >
-        //                 Contact
-        // </Link>
-        //         </li>
-        //     </ul>
-        // </div>
     )
 }
 
